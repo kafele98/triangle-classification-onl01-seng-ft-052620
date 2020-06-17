94d40e7b7d2bcf737239545c4003f1167204f74c
Zoe Keller <keller.zoe98@gmail.com>
2020-06-17 15:18:26 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  
+  class TriangleError < StandardError
+    
+  end
+  
+  
+  
+  def initialize(x, y, z)
+    
+    @x = x
+    @y = y
+    @z = z
+    if (@x + @y) <= @z || (@y + @z) <= @x || (@x + @z) <= @y
+      raise TriangleError        
+    end
+
+  end
+
+  def kind
+    if @x == @y && @y == @z
+      :equilateral
+    elsif @x == @y || @x == @z || @y == @z
+      :isosceles
+    else
+      :scalene 
+    end
+
+  end
 end
