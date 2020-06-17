055fd9d885357d3c6fcfeac89e5bf9b1bfa96b72
Zoe Keller <keller.zoe98@gmail.com>
2020-06-17 15:18:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,36 +1,31 @@
-class TriangleError < StandardError 
-end 
-
 class Triangle
-  def initialize(s1, s2, s3)
-    @sides = [s1, s2, s3]
-
-    if @sides.any? {|side| side <= 0}
-      raise TriangleError.new, "Cannot create a triangle "
-    end
-
-    @sides.combination(2).each do |combination|
-      last_side = @sides.clone
-      combination.each {|item| last_side.delete(item)}
-      if !last_side.empty? && combination.inject(:+) <= last_side.first
-        raise TriangleError.new, "Cannot create a triangle"
-      end
-    end
-
-    classify
+  # write code here
+  
+  class TriangleError < StandardError
+    
   end
-
-  def classify
-    if @sides.uniq.count == 1
-      @kind = :equilateral
-    elsif @sides.uniq.count == 2
-      @kind = :isosceles
-    else
-      @kind = :scalene
+  
+  
+  
+  def initialize(x, y, z)
+    
+    @x = x
+    @y = y
+    @z = z
+    if (@x + @y) <= @z || (@y + @z) <= @x || (@x + @z) <= @y
+      raise TriangleError        
     end
+
   end
 
   def kind
-    @kind
+    if @x == @y && @y == @z
+      :equilateral
+    elsif @x == @y || @x == @z || @y == @z
+      :isosceles
+    else
+      :scalene 
+    end
+
   end
 end
